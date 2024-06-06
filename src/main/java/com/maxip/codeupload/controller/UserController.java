ddd93d3e2f5d76b2277cd0b6b1cae6f7d3a1c225
Maksim Petrushin <petrushin.maksim001@gmail.com>
2024-06-06 15:36:06 -0400

    added react (vite) frontend, opened api to frontend origin. Fetch users api works

--- src/main/java/com/maxip/codeupload/controller/UserController.java
+++ src/main/java/com/maxip/codeupload/controller/UserController.java
@@ -3,14 +3,16 @@ package com.maxip.codeupload.controller;
 import com.maxip.codeupload.persistence.entity.UserProfile;
 import com.maxip.codeupload.service.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1")
+@CrossOrigin("http://localhost:5173/")
 public class UserController
 {
     private final UserProfileService userProfileService;
@@ -25,5 +27,13 @@ public class UserController
     public List<UserProfile> getUserProfiles()
     {
         return userProfileService.getAllUserProfiles();
+    }
+
+    @PostMapping(path = "{userProfileId}/file/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public void uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId, MultipartFile file)
+    {
+        userProfileService.uploadUserProfileImage(userProfileId, file);
     }
 }
