00fb991734f7acb4bf1eba9ec9b22169fe055e2c
Maksim Petrushin <petrushin.maksim001@gmail.com>
2024-06-06 13:50:19 -0400

    wrote a service for storing a file to S3 through an input stream

---
+++ src/main/java/com/maxip/codeupload/bucket/BucketName.java
@@ -0,0 +1,22 @@
+package com.maxip.codeupload.bucket;
+
+import org.springframework.core.env.Environment;
+
+public enum BucketName
+{
+    CODE_UPLOAD("bucket-name");
+
+    private Environment environment;
+
+    private final String bucketName;
+
+    BucketName(String bucketName)
+    {
+        this.bucketName = bucketName;
+    }
+
+    public String getBucketName()
+    {
+        return bucketName;
+    }
+}
